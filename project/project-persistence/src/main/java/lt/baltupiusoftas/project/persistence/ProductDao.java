911591a159b4a4c929ed97e0dad3744a90a61456
VIKTORIJA <wiktorijaw@gmail.com>
2018-04-22 16:57:23 +0300

    add ProductDao

--- project/project-persistence/src/main/java/lt/baltupiusoftas/project/persistence/ProductDao.java
+++ project/project-persistence/src/main/java/lt/baltupiusoftas/project/persistence/ProductDao.java
@@ -2,10 +2,17 @@ package lt.baltupiusoftas.project.persistence;
 
 import lt.baltupiusoftas.project.domain.Product;
 
+import java.util.List;
+
 /**
  * Product DAO
  *
  * @author Audrius Tvarijonas
+ * & me
  */
 public interface ProductDao extends GenericDao<Product> {
+    List<Product> findBlockOfProducts(int blockSize);
+    List<Product> findAllProducts();
+    Product findById(long productId);
+    int numberOfProducts();
 }
--- project/project-persistence/src/main/java/lt/baltupiusoftas/project/persistence/impl/ProductDaoImpl.java
+++ project/project-persistence/src/main/java/lt/baltupiusoftas/project/persistence/impl/ProductDaoImpl.java
@@ -3,10 +3,41 @@ package lt.baltupiusoftas.project.persistence.impl;
 import lt.baltupiusoftas.project.domain.Product;
 import lt.baltupiusoftas.project.persistence.ProductDao;
 
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 /**
  * Product DAO
  *
  * @author Audrius Tvarijonas
+ * & me
  */
 public class ProductDaoImpl extends GenericDaoImpl<Product> implements ProductDao{
+
+    @Override
+    public List<Product> findBlockOfProducts(int blockSize) {
+        String blockOfProductsQuery = "select p" +
+                                    " from Product p" +
+                                    " order by p.lastUpdated desc";
+        TypedQuery<Product> query = entityManager.createQuery(blockOfProductsQuery, Product.class)
+                                                 .setMaxResults(blockSize);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        String allProducts = "select p from Product p ";
+        return entityManager.createQuery(allProducts, Product.class).getResultList();
+    }
+
+    @Override
+    public Product findById(long productId) {
+        return find(productId);
+    }
+
+    @Override
+    public int numberOfProducts() {
+        String numberOfProducts = "select count(p.id) from Product p";
+        return (Integer)entityManager.createNativeQuery(numberOfProducts, Product.class).getSingleResult();
+    }
 }
