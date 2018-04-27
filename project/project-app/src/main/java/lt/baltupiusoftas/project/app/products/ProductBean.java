642e78cfc2094a393c180c640c66edef4104f4a7
VIKTORIJA <wiktorijaw@gmail.com>
2018-04-27 12:25:31 +0300

    ProductBean code

--- project/project-app/src/main/java/lt/baltupiusoftas/project/app/products/ProductBean.java
+++ project/project-app/src/main/java/lt/baltupiusoftas/project/app/products/ProductBean.java
@@ -1,4 +1,36 @@
 package lt.baltupiusoftas.project.app.products;
 
+import lt.baltupiusoftas.project.domain.Product;
+import lt.baltupiusoftas.project.service.Products.ProductService;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
 public class ProductBean {
+    //paimt prekiu sarasa
+    //surasti viena preke
+    private List<Product> products;
+    private Product product;
+
+    @Inject
+    private ProductService productService;
+
+
+    @Transactional
+    public List<Product> showAllProducts(){
+        products = productService.findAll();
+        return products;
+    }
+
+    //get product by id?
+
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product){
+        this.product = product;
+    }
 }
